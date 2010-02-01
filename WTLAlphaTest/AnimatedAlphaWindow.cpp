25de68783ee2cd95044f87cb4ad9e2ea3d680af6
Ben Straub <ben@straubnet.net>
2010-01-31 21:28:41 -0800

    Fixed assertions in release mode. Still doesn't draw right.

--- WTLAlphaTest/AnimatedAlphaWindow.cpp
+++ WTLAlphaTest/AnimatedAlphaWindow.cpp
@@ -3,6 +3,12 @@
 
 using namespace Gdiplus;
 
+#define ASSERT_SUCCEEDED(expr)						\
+	do {														\
+		HRESULT __hr__ = (expr);						\
+		assert(SUCCEEDED(__hr__));						\
+	} while (0)
+
 void CAnimatedAlphaWindow::Update( )
 {
 	mGdiDrawer->Initialize(m_hWnd);
@@ -13,51 +19,51 @@ void CAnimatedAlphaWindow::OnLButtonUp( UINT nFlags, CPoint point )
 {
 	// Create a storyboard
 	CComPtr<IUIAnimationStoryboard> storyboard;
-	assert(SUCCEEDED(mAnimMgr->CreateStoryboard(&storyboard)));
+	ASSERT_SUCCEEDED(mAnimMgr->CreateStoryboard(&storyboard));
 
 	// Create a transition
 	CComPtr<IUIAnimationTransition> transition;
 	ATLTRACE("Animating to %.0f\n", mNextAnimationValue);
 	//mTransLib->CreateSmoothStopTransition(1.0,
 	//	mNextAnimationValue, &transition);
-	assert(SUCCEEDED(mTransLib->CreateAccelerateDecelerateTransition(
+	ASSERT_SUCCEEDED(mTransLib->CreateAccelerateDecelerateTransition(
 							  0.5,
 							  mNextAnimationValue,
-							  0.2, 0.8, &transition)));
+							  0.2, 0.8, &transition));
 	mNextAnimationValue = (mNextAnimationValue == 50. ? 20. : 50.);
 
 	// Add the transition
-	assert(SUCCEEDED(storyboard->AddTransition(mVar1, transition)));
+	ASSERT_SUCCEEDED(storyboard->AddTransition(mVar1, transition));
 
 	// Schedule the storyboard to animate now
-	UI_ANIMATION_SECONDS now;
-	assert(SUCCEEDED(mAnimTimer->GetTime(&now)));
-	assert(SUCCEEDED(storyboard->Schedule(now)));
+	UI_ANIMATION_SECONDS timeNow;
+	ASSERT_SUCCEEDED(mAnimTimer->GetTime(&timeNow));
+	ASSERT_SUCCEEDED(storyboard->Schedule(timeNow));
 }
 
 void CAnimatedAlphaWindow::Initialize()
 {
 	// Initialize WAM objects
-	assert(SUCCEEDED(mAnimMgr.CoCreateInstance(CLSID_UIAnimationManager)));
-	assert(SUCCEEDED(mAnimTimer.CoCreateInstance(CLSID_UIAnimationTimer)));
-	assert(SUCCEEDED(mTransLib.CoCreateInstance(CLSID_UIAnimationTransitionLibrary))); 
+	ASSERT_SUCCEEDED(mAnimMgr.CoCreateInstance(CLSID_UIAnimationManager));
+	ASSERT_SUCCEEDED(mAnimTimer.CoCreateInstance(CLSID_UIAnimationTimer));
+	ASSERT_SUCCEEDED(mTransLib.CoCreateInstance(CLSID_UIAnimationTransitionLibrary)); 
 
 	// Connect the animation manager to the timer
 	// UI_ANIMATION_IDLE_BEHAVIOR_DISABLE tells the timer to shut itself off when there is nothing to
 	// animate
 	CComQIPtr<IUIAnimationTimerUpdateHandler> spTimerUpdateHandler(mAnimMgr);
-	assert(SUCCEEDED(mAnimTimer->SetTimerUpdateHandler(spTimerUpdateHandler,
-		UI_ANIMATION_IDLE_BEHAVIOR_DISABLE)));
+	ASSERT_SUCCEEDED(mAnimTimer->SetTimerUpdateHandler(spTimerUpdateHandler,
+		UI_ANIMATION_IDLE_BEHAVIOR_DISABLE));
 
 	// Now connect the timer to this window
 	CComObject<AnimTimerEventHandler> *evthandler;
-	assert(SUCCEEDED( CComObject<AnimTimerEventHandler>::CreateInstance(&evthandler)));
+	ASSERT_SUCCEEDED( CComObject<AnimTimerEventHandler>::CreateInstance(&evthandler));
 	evthandler->Initialize(boost::bind(UpdateCallback, this));
 	CComQIPtr<IUIAnimationTimerEventHandler> spTimerEventHandler(evthandler);
-	assert(SUCCEEDED(mAnimTimer->SetTimerEventHandler(spTimerEventHandler)));
+	ASSERT_SUCCEEDED(mAnimTimer->SetTimerEventHandler(spTimerEventHandler));
 
 	// Create animation variable(s)
-	assert(SUCCEEDED(mAnimMgr->CreateAnimationVariable(50, &mVar1)));
+	ASSERT_SUCCEEDED(mAnimMgr->CreateAnimationVariable(50, &mVar1));
 
 	// Drawing methods
 	mGdiDrawer->Initialize(m_hWnd);
